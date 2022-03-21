930fbd8d78c4f22eb638c685a8991307ba382d33
Rildo Franco <rildowilliam2017@gmail.com>
2022-03-21 15:39:19 +0000

    init

--- notaipilmobile/lib/parts/variables.dart
+++ notaipilmobile/lib/parts/variables.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:notaipilmobile/configs/size_config.dart';
 
+/**Strings */
 final String baseImageUrl = "http://10.0.2.2:9800/api/v1/profile/";
+final String fontFamily = 'Roboto';
+
+/**Colors */
 final Color backgroundColor = Color(0xFFF5F6FA);
-//final Color backgroundColor = Color.fromARGB(100, 253, 254, 255);
 final Color fillColor = Color(0xFFF5F6FA);
 final Color letterColor = Color(0xFF333333);
 final Color linKColor = Color(0xFF0D89A4);
@@ -11,7 +14,11 @@ final Color appBarColor = Colors.white;
 final Color drawerColor = Colors.white;
 final Color borderAndButtonColor = Color(0xFF0D89B0);
 final Color iconColor = Colors.black;
-final DateTime now = DateTime.now();
-final double titleSize = SizeConfig.textMultiplier !* 4.5;
-final String fontFamily = 'Roboto';
 final Color teacherNameColor = Colors.red;
+//final Color backgroundColor = Color.fromARGB(100, 253, 254, 255);
+
+/**DateTime */
+final DateTime now = DateTime.now();
+
+/**Double */
+final double titleSize = SizeConfig.textMultiplier !* 4.5;
